a2fa5f0fbc1a3b01a56e72d9111f8475d2ba101f
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-27 11:47:17 -0300

    test: add test to elementary functions

---
+++ tests/test_elementary_functions.py
@@ -0,0 +1,41 @@
+import pytest
+from challenges.second_challenge_elementary_functions import math
+
+
+def test_add_function():
+    assert math['add']([2, 3, 5, 7, 9]) == 26
+    assert math['add']([20, -4, 7, 6, 2]) == 31
+    assert math['add']([1]) == 1
+    assert math['add']([]) == 0
+
+    with pytest.raises(ValueError):
+        math['add'](12)
+
+
+def test_sub_function():
+    assert math['sub']([8, 2, 1, 3, 5]) == -3
+    assert math['sub']([20, -4, 7, 6, 2]) == 9
+    assert math['sub']([1]) == 1
+    assert math['sub']([]) == 0
+
+    with pytest.raises(ValueError):
+        math['sub'](12)
+
+
+def test_div_function():
+    assert math['div'](9, 3) == 3
+    assert math['div'](22, 2) == 11
+    assert math['div'](0, 12) == 0
+
+    with pytest.raises(ValueError):
+        assert math['div'](3, 0) == 0
+
+
+def test_mul_function():
+    assert math['mul']([3, 4, 2, 9]) == 216
+    assert math['mul']([11, 2, 7, 6]) == 924
+    assert math['mul']([1]) == 1
+    assert math['mul']([]) == 0
+
+    with pytest.raises(ValueError):
+        assert math['mul'](12)
